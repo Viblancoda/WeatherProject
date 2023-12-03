6111aa633c2a81e82ba8c0addb7219ae93ba4986
Victor <vitukodb@gmail.com>
2023-12-03 15:08:27 +0000

    Created directory

--- event_store_builder/src/main/java/event_store_builder/JMRWeatherStore.java
+++ event_store_builder/src/main/java/event_store_builder/JMRWeatherStore.java
@@ -1,22 +1,20 @@
 package event_store_builder;
 
+import com.google.gson.*;
 import dacd.blanco.model.Weather;
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializer;
-
 import java.time.Instant;
 
 public class JMRWeatherStore {
+
     private final String url;
     private final String topicName;
     private final String clientId;
+    private static final String directoryPath = "messages_directory";
 
-    public JMRWeatherStore(String url, String topicName, String clientId) {
+    public JMRWeatherStore(String url, String topicName, String clientId, String directoryPath) {
         this.url = url;
         this.topicName = topicName;
         this.clientId = clientId;
@@ -64,12 +62,13 @@ public class JMRWeatherStore {
             }
         }
     }
+
     public static void main(String[] args) {
         String url = "tcp://localhost:61616";
         String topicName = "prediction.Weather";
         String clientId = "exampleClientId";
 
-        JMRWeatherStore weatherStore = new JMRWeatherStore(url, topicName, clientId);
+        JMRWeatherStore weatherStore = new JMRWeatherStore(url, topicName, clientId, directoryPath);
         weatherStore.receiveMessage();
     }
 }
